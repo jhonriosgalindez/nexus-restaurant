8d34e9db220186720a5efd72e88328904a00be62
Jhon Rios <r.jhonf@gmail.com>
2023-08-22 12:00:48 -0500

    header component complete

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'marcellus': ['Marcellus']
+			},
+			keyframes: {
+				show: {
+					'0%': { opacity: '0' },
+					'100%': { opacity: '1' }
+				}
+			}
+		},
 	},
 	plugins: [],
 }
